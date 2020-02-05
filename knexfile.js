10957cac5f7b62adfe736d330d4d476e27f29484
marielapg <mpgonzales77@gmail.com>
2020-02-04 18:14:54 -0800

    adding to knexfile

--- knexfile.js
+++ knexfile.js
@@ -30,7 +30,8 @@ module.exports = {
   },
 
   production: {                                                        
-    client: 'pg',                                                                                                  
+    client: 'pg', 
+    useNullAsDefault: true,                                                                                                 
     connection: process.env.DATABASE_URL,                               
     pool: {                                                            
       min: 2,                                                           
